dd04543d7681f84fb838e3d9736255fa8b82fa29
Samera Zaidi <samera.zaidi@gmail.com>
2017-01-26 10:42:01 -0800

    $  Changes to be committed:

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
 })
 
 /*
@@ -10,9 +11,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+	$('.jumbotron h1').text("How did I survive until now?");
+    $("#testjs").text("Please wait...");
+    $(".jumbotron p").addClass("active");
 	});
+
+	
+$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e){
+		e.preventDefault();
+		 var containingProject = $(this).closest(".project");
+    	 var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+      $("a.thumbnail").fadeOut();
+
+	}
+}
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
-}
